1857a133a1fe7e504954eb27d94ab7eb0b34058a
vilison <d.vilison@gmail.com>
2015-11-24 03:15:57 +0800

    add user routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,13 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+//Blade::setContentTags('@{{', '}}');         // for variables and all things Blade
+//Blade::setEscapedContentTags('<%%', '%%>');     // for escaped data
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user/index/{status?}','UserController@index');
+Route::post('user/store','UserController@store');
+Route::post('user/leave','UserController@leave');
+Route::post('user/upwd','UserController@upwd');
