077d7773021c52a43cb19fbf5863dd4fd215c6d5
vilison <d.vilison@gmail.com>
2015-11-26 00:48:12 +0800

    update nav

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -14,10 +14,11 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">工单浏览</a></li>
-                <li><a href="#">用户管理</a></li>
+                <li class="active"><a href="/order/index">工单浏览</a></li>
+                <li><a href="#">工单查询</a></li>
+                <li><a href="/user/index/">用户管理</a></li>
                 <li><a href="#">系统配置</a></li>
-                <li><a href="#">日志查看</a></li>
+                <li><a href="/log/index/">日志查看</a></li>
             </ul>
 
         </div><!-- /.navbar-collapse -->
